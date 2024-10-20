45753906503ba5b777f55584aeb4a2713802995c
Hoanron <lieuhoanglong.91@gmail.com>
2024-10-20 21:03:52 +0700

    commit

--- src/Gui/KhuyenMai.java
+++ src/Gui/KhuyenMai.java
@@ -116,8 +116,6 @@ public class KhuyenMai extends javax.swing.JPanel {
                     if (!selectedprodIDs.contains(id)) {
                         selectedprodIDs.add(id);
                     }
-                } else {
-                    selectedprodIDs.remove(id);
                 }
             }
         });
@@ -265,7 +263,7 @@ public class KhuyenMai extends javax.swing.JPanel {
         Chk_ApDung.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!Chk_ApDung.isSelected()) {
+                if (Chk_ApDung.isSelected()) {
                     tbl_SanPham.setEnabled(true);
                     tbl_SanPham.setFocusable(true);
                     tbl_SanPham.getTableHeader().setEnabled(true);
@@ -601,6 +599,8 @@ public class KhuyenMai extends javax.swing.JPanel {
         if (success) {
             JOptionPane.showMessageDialog(null, "Thêm khuyến mãi thành công!");
             inPromtionProducts.clear();
+            selectedprodIDs.clear();
+            selectedprodTypeIDs.clear();
             hienThi();
         } else {
             JOptionPane.showMessageDialog(null, "Thêm khuyến mãi thất bại!");
@@ -676,6 +676,8 @@ public class KhuyenMai extends javax.swing.JPanel {
         if (success) {
             JOptionPane.showMessageDialog(null, "Sửa khuyến mãi thành công!");
             inPromtionProducts.clear();
+            selectedprodIDs.clear();
+            selectedprodTypeIDs.clear();
             hienThi();
         } else {
             JOptionPane.showMessageDialog(null, "Sửa khuyến mãi thất bại!");
